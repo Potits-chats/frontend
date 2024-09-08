1858c429df72c6b7e75ae0c15fb0b5680df43ff2
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-09-08 12:41:03 +0200

    Remove animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,6 @@ import { AppService } from './services/app.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('routerTransition', [
-      transition('* <=> *', [
-        query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
-        group([
-          query(':enter', [
-            style({ transform: 'translateX(100%)' }),
-            animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
-          ], { optional: true }),
-          query(':leave', [
-            style({ transform: 'translateX(0%)' }),
-            animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
-          ], { optional: true }),
-        ]),
-      ]),
-    ]),
-    
-  ],
 })
 export class AppComponent {
   message = '';
